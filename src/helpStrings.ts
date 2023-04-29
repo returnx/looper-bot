ffb36718c7a375b508718c437d83ea1933eb498b
Akshay Hagaragi <contact@akshayy.com>
2023-04-29 18:13:25 +0530

    Update help strings

--- src/helpStrings.ts
+++ src/helpStrings.ts
@@ -1,125 +1,10 @@
-const mana = `\`\`\` 
-First,
+const mana = "Mana help is here https://discord.com/channels/852104066401173514/984054952324370442/985574700300255252";
 
-Use the calculator to check Forbidden rite damage, it must be less than your ward
-If not, then remove Mind Over Matter Keystone. Because FR damage will delete your mana with MoM keystone.
+const rareRing = "Ring help is here https://discord.com/channels/852104066401173514/984054952324370442/1053981220716949544";
 
-Second,
+const upgrade = "Upgrade list https://discord.com/channels/852104066401173514/984054952324370442/1054796088198053939";
 
-In your helmet, the first socket should be blue and put Summon skeleton is that socket
-
-Third,
-
-If you are using rare ring
-Use gem links FR level 1 + CWDT 12
-No exceptions, always use these in the gloves
-
-Fourth,
-
-Put your skill gems in the last socket of weapon and body armour.
-The skill gems should not be first gem in weapon and body, put support gems first!
-
-Fifth,
-
-Get reduced cost of mana skills boots enchant, also reduced mana cost on tree. Last Option.
-
-Sixth,
-
-Don't use too many skills in loop
-
-\`\`\``;
-
-const rareRing = `\`\`\` 
-How to Use Rare Ring In 3.21
-
-This will require 3 divergent CWDTs 
-One for Body, One for Weapon and One for Helm.
-
-Minimum Ward - 1200 
-Ideal - 1250
-Endgame - 1350
-
-Required Level 21 Summon Skeletons for 4 Skeletons - 4x420 
-
-Anomalous Minion Speed
-        Quality - 20%
-        Level - Does Not Matter
-
-Anomalous Summon Skeletons
-        Quality - 3% or more
-        Level - 20/21
-
-Option 1 - Low Budget
-
-Helm - Divergent CWDT 20/20 + Anomalous Summon Skeletons Level 20 + Empower 2 + Anomalous Minion Speed x/20
-Gloves - FR level 1 + Normal CWDT 12
-Body - Divergent 19/20
-Weapon - Divergent 20/20
-
-Option 2 -- Higher Budget
-
-Ashes Of The Stars/ Rare Amulet  +1 to level of All Skill Gems 
-Helm - Divergent CWDT 20/20 + Anomalous Summon Skeletons Level 20 + Anomalous Minion Speed x/20 + Snipers Mark 20
-Gloves - FR Level 1 + Normal CWDT 12
-Body - Divergent 19/20
-Weapon - Divergent 20/20
-
-End Game -- Ultimate Budget
-
-Option 3
-
-Divergent CWDT 21/20  + Anomalous Summon Skeletons level 21 + Anomalous Minion Speed 1/20
-Gloves - FR level 1 + CWDT 12
-Body - Divergent 19/20
-Weapon - Divergent 21/20
-
-\`\`\``;
-
-const upgrade = `\`\`\`
-How To Upgrade For More  Damage
-
-Min Maxed Pandamonious PoB - https://pobb.in/hP8_OzkGNd6P
-Min Maxed Ashes PoB -  https://pobb.in/L7SaA2XyqP3f
-
-Upgrade In Any Order!
-
-    -- Level 100 - 5 Ways Carry
-    -- All Damage and Support Gems 20% quality
-    -- Awakened Support Gems
-    -- Rare Jewels With Crit / Spell Damage -- Use the link in forum guide
-    -- Divergent Righteous Fire, Phantasmal Sigil of Power
-    -- Ward items with High Resistance Suffix
-    -- 4 Flask Upgrade Use Dying Sun 4th flask
-    -- Weapon Harvest Enchant Quality For Elemental Damage
-    -- Watcher's With Cold Pen or Cold Damage
-    -- Watcher's With Plus Crit Damage for Hatred
-    -- Large cluster upgrade with 3 notables
-    -- A Good Timeless Jewel
-    -- Rare Ring Upgrade
-    -- 27% CDR / 52% CDR
-    -- Heartbound Rings With Better Corrupted Implicits
-    -- Ashes Of The Stars / +2 Rare Amulet with Crit Multi Suffix
-    -- Helm Enchant Either RF or Ice Spear damage
-    -- Boots Enchant Cold damage when hit
-    -- Belt Lab Enchant
-    -- Whispers of Doom Anonint on Amulet, self cast Frostbite
-
-Please see the Min Maxed PoB to see how its done
-Save 150  Divine Switch to Omni POB, Check out the 300 Divines Omni PoB how to do it
-Omni PoB -> https://pobb.in/Smfr5iIizeBs 
-Omni tress are squishy, Omni Spell Suppress is good.
-
-Good luck! Don't be Afraid! Go Bold!
-\`\`\``;
-
-const tanky = `\`\`\`
-Incorporte the following in your build
-
-Spell Suppression, Progenesis, 3k life, 1200 Ward, Ailment immunity, Curse Immunity, Chaos Resistance
-Optional -  Attack Block, Evasion with Grace Aura
-
-\`\`\`
-See #example-pob`;
+const tanky = "Tanky list https://discord.com/channels/852104066401173514/984054952324370442/986532963174973441";
 
 const help = `\`\`\`yaml
 -------BOT COMMANDS-------
@@ -131,6 +16,7 @@ help mana       # Mana fix help
 help ring       # Help to use rare ring
 help tanky      # How to become tanky
 help pob        # How to configure pob
+help cdr        # Help on CDR
 
 \`\`\`
 Post Your PoB in https://discord.com/channels/852104066401173514/988474586490368110
